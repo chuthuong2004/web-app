64e58440eec7cc8413b369e09026504f3f3527c6
longorlu2001 <19h1120052@sv.ut.edu.vn>
2022-04-13 16:04:58 +0700

    product

--- client/react-website-slyder/src/components/ProductView.jsx
+++ client/react-website-slyder/src/components/ProductView.jsx
@@ -10,11 +10,14 @@ const ProductView = (props) => {
     <div className="product">
         <div className="product__images">
             <div className="product__images__list">
-                <div className="product__images__item">
-                    <img src={product.images[0].img} alt="" onClick={()=> setPreviewImg(product.images[0].img)}/>
+                <div className="product__images__list__item" onClick={()=> setPreviewImg(product.images[0].img)}>
+                    <img src={product.images[0].img} alt=""  className="product__images__list__item__img" />
                 </div>
-                <div className="product__images__item">
-                    <img src={product.images[1].img} alt=""  onClick={()=> setPreviewImg(product.images[1].img)} />
+                <div className="product__images__list__item" onClick={()=> setPreviewImg(product.images[1].img)}>
+                    <img src={product.images[1].img} alt=""   />
+                </div>
+                <div className="product__images__list__item" onClick={()=> setPreviewImg(product.images[2].img)}>
+                    <img src={product.images[2].img} alt=""   />
                 </div>
             </div>
             <div className="product__images__main">
